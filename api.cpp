68c1cb1118e0420efba424447a5a1159f2ebb1b2
nicole-angove <78260826+nicole-angove@users.noreply.github.com>
2023-02-01 19:44:35 +1100

    Add API function useAbility (#9)
    * Add API function useAbility
    
    * Addressed comments from review

---
+++ api.cpp
@@ -0,0 +1,9 @@
+#include "api.h"
+
+bool useAbility(int abilityID) {
+    if (abilityID == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
---
+++ api.h
@@ -0,0 +1,11 @@
+#ifndef API_H
+#define API_H
+
+/**
+ * Tries to use the given ability.
+ * 
+ * Returns false if the ability cannot be used.
+ */
+bool useAbility(int abilityID);
+
+#endif
